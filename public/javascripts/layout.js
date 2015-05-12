5d7bc16cf9f6038872d2b7ad4add9ac484c1615e
Mairead Buchan <mairead.buchan@headlondon.com>
2015-05-12 11:39:00 +0100

    Adding links and images into feed

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -10,7 +10,20 @@ imagesLoaded( userImgs, function() {
 	});
 });
 
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var itemTitle = document.querySelectorAll('.caption');
+	console.log(itemTitle);
+	function addLinks(text){
+		console.log("b4 link", text);
+		text = text.replace(/[a-z]+:\/\/([a-z0-9-_]+\.[a-z0-9-_:~\+#%&\?\/.=]+[^:\.,\)\s*$])/ig, function(m, link) {
+      return '<a title="' + m + '" href="' + m + '">' + ((link.length > 36) ? link.substr(0, 35) + '&hellip;' : link) + '</a>';
+    })
+	 	return text;
+	}
 
+	[].forEach.call(itemTitle, function(v){
+		console.log(v)
+		v.innerHTML = addLinks(v.innerHTML);
+	})
 
-
-
+});
--- routes/index.js
+++ routes/index.js
@@ -14,15 +14,48 @@ var client = new Twitter({
   access_token_secret: 'tkZl2glmZspHoTFjQxiXuC9Iyd1hRAjaswoYSXaQXFtEt'
 });
 
+function randomSize(){
+	return Math.round(Math.random() *10) >5 ? 'small': 'large';
+}
+
+function stripHashtags(text){
+	//console.log("b4 hash", text);
+	text = text.replace(/(^|[^&\w'"]+)\#([a-zA-Z0-9_^"^<]+)/g, '');
+ 	return text;
+}
+
+//regex help from Remy
+//https://github.com/remy/twitterlib/blob/master/twitterlib.js#L81
+
+function addLinks(text){
+	text = text.replace(/[a-z]+:\/\/([a-z0-9-_]+\.[a-z0-9-_:~\+#%&\?\/.=]+[^:\.,\)\s*$])/ig, function(m, link) {
+          return '<a title="' + m + '" href="' + m + '">' + ((link.length > 36) ? link.substr(0, 35) + '&hellip;' : link) + '</a>';
+        })
+ 	return text;
+}
+
+	
+function stripSymbols(text){
+	return text.replace("&amp;", "&")
+}
+
+function augmentTxt(text){
+	text = stripHashtags(text);
+	text = stripSymbols(text);
+	//text = addLinks(text);
+	console.log("returned", text);
+	return text;
+}
+
+
 function filterThumbnails(data){
 	var instagrams = [];
 	data.forEach(function(item){
-		var randomSize = Math.round(Math.random() *10) >5 ? 'small': 'large';
 		instagrams.push({
 			imgUrl:item.images.low_resolution.url, 
-			txt:item.caption.text, 
+			txt: augmentTxt(item.caption.text), 
 			type: 'instagram', 
-			imgSize: randomSize,
+			imgSize: randomSize(),
 			link: item.link,
 			user: item.caption.from.username
 		});
@@ -30,15 +63,29 @@ function filterThumbnails(data){
 	return instagrams;
 }
 
+
+
 function filterTwitterText(tweets){
 	var tweetText = [];
 	
 	tweets.forEach(function(item){
-		tweetText.push({
-			txt:item.text,
-			 type: 'tweet',
-			 user: item.user.screen_name
+		if (item.entities.media){
+			tweetText.push({
+				imgUrl: item.entities.media[0].media_url,
+				txt: augmentTxt(item.text),
+			 	type: 'instagram',
+			 	imgSize: randomSize(),
+			 	link: item.entities.media[0].url,
+			 	user: item.user.screen_name
 			});
+		}else{
+			tweetText.push({
+				txt: augmentTxt(item.text),
+			 	type: 'tweet',
+			 	user: item.user.screen_name
+			});
+
+		}
 	});
 	return tweetText;
 }
@@ -46,7 +93,9 @@ function filterTwitterText(tweets){
 function getNewContent(res){
 
 	var instagramRequest = new Promise(function(resolve,reject){
+
     instagram.tags.recent({
+    	min_tag_id: '968207301261607073_38812255',
 		  name: 'eight17',
 		  complete: function(data){
 		    var instagrams = filterThumbnails(data.slice(0,2));
@@ -56,8 +105,13 @@ function getNewContent(res){
 	});
 
 	var twitterRequest = new Promise(function(resolve,reject){
-		client.get('search/tweets', {'q':'#eight17', 'count': '10'}, function(error, tweets, response){
+		client.get('search/tweets', {
+			'q':'#eight17',
+			'count': '10'
+			// 'since_id': 
+			}, function(error, tweets, response){
 		  if (!error) {
+
 		  	var tweetText = filterTwitterText(tweets.statuses);
 		    resolve(tweetText);
 		  }
